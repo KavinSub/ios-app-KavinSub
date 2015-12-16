729b08c0504d80be1ad5a9cada3ffe7c831ed9e2
Kavin Subramanyam <kavin.subramanyam@students.makeschool.com>
2015-12-15 19:12:00 -0800

    began email sign up implementation

---
+++ Exchange/ViewControllers/EmailSignUpViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EmailSignUpViewController.swift
+//  Exchange
+//
+//  Created by Kavin Subramanyam on 12/15/15.
+//  Copyright © 2015 Kavin Subramanyam. All rights reserved.
+//
+
+import Cocoa
+
+class EmailSignUpViewController: UIViewController {
+
+}
