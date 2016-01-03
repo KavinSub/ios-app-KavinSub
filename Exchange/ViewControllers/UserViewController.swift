4e86abe8bf6af68122354a64031fffcebcbd32fd
Kavin Subramanyam <kavin.subramanyam@students.makeschool.com>
2016-01-02 22:55:19 -0800

    Animate views in profile view

--- Exchange/ViewControllers/UserViewController.swift
+++ Exchange/ViewControllers/UserViewController.swift
@@ -17,11 +17,41 @@ class UserViewController: UIViewController {
     
     @IBOutlet weak var contactInfoView: UIView!
     
+    @IBOutlet weak var linkedInView: UIView!
+    
     @IBOutlet weak var aboutTextView: UITextView!
+    
+    @IBOutlet weak var otherLinksView: UIView!
     
     @IBOutlet weak var backButton: UIButton!
     
     @IBOutlet weak var editButton: UIButton!
+    
+    // Constraints related to animations
+    var backTrailingConstraint: NSLayoutConstraint?
+    
+    // Toggles edit mode, changes UI components
+    @IBAction func toggleEditMode(sender: AnyObject) {
+        if inEditingMode{
+            inEditingMode = false
+            
+            self.disableButtons()
+            outOfEditModeAnimations()
+            
+            editButton.setTitle("Edit", forState: UIControlState.Normal)
+            
+            aboutTextView.editable = false
+        }else{ // Put UI into edit mode
+            inEditingMode = true
+            
+            self.disableButtons()
+            intoEditModeAnimations()
+            
+            editButton.setTitle("Done", forState: UIControlState.Normal)
+            
+            aboutTextView.editable = true
+        }
+    }
     
     // Goes back to the previous view controller
     @IBAction func goBack(sender: AnyObject){
@@ -31,12 +61,16 @@ class UserViewController: UIViewController {
     // If true, edit button is present
     var allowsEditMode: Bool?
     
+    var inEditingMode: Bool = false
+    
     // User that is to be displayed on this view controller
     var user: PFUser?
     
     override func viewDidLoad(){
+        setupGestures()
         displayUser(user!)
         changeButtons()
+        
     }
     
     func displayUser(user: PFUser){
@@ -72,9 +106,15 @@ class UserViewController: UIViewController {
             })
         }
         
-        // Change contact info view
+        // Change info views
         contactInfoView.layer.cornerRadius = contactInfoView.frame.size.width/2
         contactInfoView.clipsToBounds = true
+        
+        linkedInView.layer.cornerRadius = linkedInView.frame.size.width/2
+        linkedInView.clipsToBounds = true
+        
+        otherLinksView.layer.cornerRadius = otherLinksView.frame.size.width/2
+        otherLinksView.clipsToBounds = true
         
         // Add content to text view
         if let text = user.valueForKey("about") as! String?{
@@ -88,10 +128,10 @@ class UserViewController: UIViewController {
         if allowsEditMode!{
             // Add constraints to back button
             let backTopConstraint = NSLayoutConstraint(item: backButton, attribute: .Top, relatedBy: .Equal, toItem: self.view, attribute: .TopMargin, multiplier: 1.0, constant: 24.0)
-            let backTrailingConstraint = NSLayoutConstraint(item: backButton, attribute: .Trailing, relatedBy: .Equal, toItem: self.view, attribute: .TrailingMargin, multiplier: 1.0, constant: 0.0)
+            backTrailingConstraint = NSLayoutConstraint(item: backButton, attribute: .Trailing, relatedBy: .Equal, toItem: self.view, attribute: .TrailingMargin, multiplier: 1.0, constant: 0.0)
             
             self.view.addConstraint(backTopConstraint)
-            self.view.addConstraint(backTrailingConstraint)
+            self.view.addConstraint(backTrailingConstraint!)
             
             // Add constraints to the back button
             let editTopConstraint = NSLayoutConstraint(item: editButton, attribute: .Top, relatedBy: .Equal, toItem: self.view, attribute: .TopMargin, multiplier: 1.0, constant: 24.0)
@@ -101,7 +141,98 @@ class UserViewController: UIViewController {
             self.view.addConstraint(editLeadingConstraint)
         }
     }
+    
+    // Sets up gestures for this view controller
+    func setupGestures(){
+        let tapScreenGesture = UITapGestureRecognizer(target: self, action: Selector("closeKeyboards"))
+        self.view.addGestureRecognizer(tapScreenGesture)
+    }
+    
+    // Close open keyboards
+    func closeKeyboards(){
+        self.view.endEditing(true)
+    }
+    
+    func outOfEditModeAnimations(){
+        
+        // Animate back button
+        self.backButton.hidden = false
+        
+        self.backTrailingConstraint?.constant = 0.0
+        self.view.setNeedsUpdateConstraints()
+        let backButtonAnimation = { () -> Void in
+            self.view.layoutIfNeeded()
+        }
+        UIView.animateWithDuration(0.5, animations: backButtonAnimation) { (success: Bool) -> Void in
+            if success{
+                self.enableButtons()
+            }
+        }
+        
+        // Animate info views
+        // i) linkedInView
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.linkedInView.transform = CGAffineTransformIdentity
+        }
+        // ii) contactInfoView
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.contactInfoView.transform = CGAffineTransformIdentity
+        }
+        // iii) otherLinksView
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.otherLinksView.transform = CGAffineTransformIdentity
+        }
+    }
+    
+    func intoEditModeAnimations(){
+        // Animate back button
+        self.backTrailingConstraint?.constant += (self.view.frame.width - self.backButton.center.x) + self.backButton.frame.width
+        self.view.setNeedsUpdateConstraints()
+        let backButtonAnimation = { () -> Void in
+            self.view.layoutIfNeeded()
+        }
+        
+        UIView.animateWithDuration(0.5, animations: backButtonAnimation) { (success: Bool) -> Void in
+            if success{
+                self.backButton.hidden = true
+                self.enableButtons()
+            }
+        }
+        
+        // Animate info views
+        // i) linkedInView
+        horizontalTranslateRotateAnimation(self.linkedInView, duration: 0.5, delay: 0.0, tx: -240, degrees: 180, completion: nil)
+        // ii) contactInfoView
+        horizontalTranslateRotateAnimation(self.contactInfoView, duration: 0.5, delay: 0.0, tx: -480, degrees: 180, completion: nil)
+        // iii) otherLinksView
+        horizontalTranslateRotateAnimation(self.otherLinksView, duration: 0.5, delay: 0.0, tx: 240, degrees: 180, completion: nil)
+    }
+    
+    func disableButtons(){
+        self.editButton.enabled = false
+        self.backButton.enabled = false
+    }
+    
+    func enableButtons(){
+        self.editButton.enabled = true
+        self.backButton.enabled = true
+    }
+    
+    func degreesToRadians(degrees: CGFloat) -> CGFloat{
+        let PIFloat = CGFloat(M_PI)
+        return (PIFloat * degrees / 180.0) as CGFloat
+    }
+    
+    // Performs a horizontal translation, and rotation on the given view
+    func horizontalTranslateRotateAnimation(view: UIView, duration: Double, delay: Double, tx: CGFloat, degrees: CGFloat, completion: ((Bool) -> Void)?) -> CGAffineTransform{
+        
+        let animation = {
+            let translate = CGAffineTransformTranslate(view.transform, tx, 0.0)
+            view.transform = CGAffineTransformRotate(translate, self.degreesToRadians(degrees))
+        }
+        
+        UIView.animateWithDuration(duration, delay: delay, options: UIViewAnimationOptions.CurveLinear, animations: animation, completion: completion)
+        
+        return view.transform
+    }
 }
-
-
-
--- Exchange/Views/UIElementProperties.swift
+++ Exchange/Views/UIElementProperties.swift
@@ -13,4 +13,5 @@ class UIElementProperties{
     
     static let textColor = UIColor(red: 249.0/255.0, green: 242.0/255.0, blue: 231.0/255.0, alpha: 1.0)
     static let connectionStatus = UIColor(red: 174.0/255.0, green: 226.0/255.0, blue: 57.0/255.0, alpha: 1.0)
+    static let backgroundColor = UIColor(red: 0.0/255.0, green: 168.0/255.0, blue: 198/255.0, alpha: 1.0)
 }
