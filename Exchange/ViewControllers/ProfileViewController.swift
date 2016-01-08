ab55c895bb2987af9b9f6df484365bdd230cf4a5
Kavin Subramanyam <kavin.subramanyam@students.makeschool.com>
2016-01-07 19:49:00 -0800

    Add fields Position, Company. Animate said fields.

--- Exchange/ViewControllers/ProfileViewController.swift
+++ Exchange/ViewControllers/ProfileViewController.swift
@@ -27,12 +27,53 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var editButton: UIButton!
     
+    @IBOutlet weak var positionField: UITextField!
+    
+    @IBOutlet weak var companyField: UITextField!
+    
+    // text fields dict
+    var textFields: [Int: String]?
+    
     var inEditMode: Bool = false
     
     // Everything related to edit view
     
+    @IBOutlet weak var positionLeading: NSLayoutConstraint!
+    
+    @IBOutlet weak var companyLeading: NSLayoutConstraint!
+    
     @IBAction func showEdit(sender: AnyObject) {
-        
+        if !inEditMode{
+            inEditMode = true
+            
+            editButton.setTitle("Done", forState: UIControlState.Normal)
+            
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+                self.jobLabel.transform = CGAffineTransformTranslate(CGAffineTransformIdentity, -1.0 * (self.view.frame.width/2.0 + self.jobLabel.frame.width), 0.0)
+                
+                self.positionLeading.constant =  -1.0 * (self.view.frame.width - ((self.view.frame.width - (2 * self.positionField.frame.width + self.companyLeading.constant))/2.0))
+                
+                self.view.layoutIfNeeded()
+            })
+        }else{
+            inEditMode = false
+            
+            endEditing()
+            
+            editButton.setTitle("Edit", forState: UIControlState.Normal)
+            
+            displayUser()
+            
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+                self.jobLabel.transform = CGAffineTransformIdentity
+                
+                self.positionLeading.constant = 0.0
+                
+                self.view.layoutIfNeeded()
+            })
+            
+            user.saveInBackground()
+        }
     }
     
     // Everything related to the info view
@@ -71,7 +112,16 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad(){
         
         UIChanges()
+        addGestures()
         setNeedsStatusBarAppearanceUpdate()
+        
+        positionField.delegate = self
+        positionField.tag = 0
+        
+        companyField.delegate = self
+        companyField.tag = 1
+        
+        textFields = [positionField.tag: "position", companyField.tag: "company"]
         
         displayUser()
     }
@@ -106,12 +156,17 @@ class ProfileViewController: UIViewController {
         let position = (user.valueForKey("position") as! String?) ?? ""
         let company = (user.valueForKey("company") as! String?) ?? ""
         if position == "" && company == ""{
-            jobLabel.text = ""
+            jobLabel.text = "Works at"
         }else if position == ""{
             jobLabel.text = "Works at \(company)"
-        }else{
+        }else if company == ""{
             jobLabel.text = "Works as \(position)"
+        }else{
+            jobLabel.text = "\(position) at \(company)"
         }
+        
+        positionField.text = position
+        companyField.text = company
         
         // iv) contact info
         phoneNumberLabel.text = (user.valueForKey("phoneNumber") as! String?) ?? ""
@@ -132,4 +187,22 @@ class ProfileViewController: UIViewController {
         aboutTextView.editable = false
     }
     
+    func addGestures(){
+        let tapScreenGesture = UITapGestureRecognizer(target: self, action: Selector("endEditing"))
+        self.view.addGestureRecognizer(tapScreenGesture)
+    }
+    
+    func endEditing(){
+        self.view.endEditing(true)
+    }
+    
+}
+
+extension ProfileViewController: UITextFieldDelegate{
+    func textFieldDidEndEditing(textField: UITextField) {
+        let key = textFields![textField.tag]!
+        let value = textField.text
+        
+        user.setValue(value, forKey: key)
+    }
 }
